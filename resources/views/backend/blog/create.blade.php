d5eb07884587af3cf7966b0370b6f94cdc03ef0f
Manu PC win8 <perudragon@hotmail.com>
2020-06-02 10:29:56 -0500

    4.7 Slug Creator

--- resources/views/backend/blog/create.blade.php
+++ resources/views/backend/blog/create.blade.php
@@ -120,7 +120,7 @@
                             <div class="card-footer">
 
                                 {!! Form::submit('Create new post', ['class' => 'btn btn-primary']) !!}
-                                {{--<button type="submit" class="btn btn-primary">Submit</button>--}}
+                                
                             </div>
                             {{Form::close()}}
 
@@ -138,40 +138,5 @@
     </div>
 @endsection
 
-@section('style')
 
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="{{ asset('backend/css/ionicons.min.css') }}">
-    <!-- DataTables -->
-    <link rel="stylesheet" href="{{ asset('backend/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
-
-@endsection
-@section('script')
-
-    <!-- DataTables -->
-    <script src="{{ asset('backend/plugins/datatables/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('backend/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-    <script src="{{ asset('backend/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
-    <script src="{{ asset('backend/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
-    <!-- AdminLTE App -->
-    <!-- AdminLTE for demo purposes -->
-    {{--<script src="../../dist/js/demo.js"></script>--}}
-    <script>
-        $(function () {
-            $("#example1").DataTable({
-                "responsive": true,
-                "autoWidth": false,
-            });
-            $('#example2').DataTable({
-                "paging": true,
-                "lengthChange": false,
-                "searching": false,
-                "ordering": true,
-                "info": true,
-                "autoWidth": false,
-                "responsive": true,
-            });
-        });
-    </script>
-@endsection
+@include('backend.blog.scripts')
---
+++ resources/views/backend/blog/scripts.blade.php
@@ -0,0 +1,53 @@
+
+
+@section('style')
+
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="{{ asset('backend/css/ionicons.min.css') }}">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="{{ asset('backend/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+
+@endsection
+@section('script')
+
+    <!-- DataTables -->
+    <script src="{{ asset('backend/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('backend/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('backend/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('backend/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+    <!-- AdminLTE App -->
+    <!-- AdminLTE for demo purposes -->
+    {{--<script src="../../dist/js/demo.js"></script>--}}
+    <script>
+        $(function () {
+            $("#example1").DataTable({
+                "responsive": true,
+                "autoWidth": false,
+            });
+            $('#example2').DataTable({
+                "paging": true,
+                "lengthChange": false,
+                "searching": false,
+                "ordering": true,
+                "info": true,
+                "autoWidth": false,
+                "responsive": true,
+            });
+        });
+
+        <!-- create a slug opens -->
+        $('#title').on('blur', function() {
+            var theTitle = this.value.toLowerCase().trim(),
+                slugInput = $('#slug'),
+                theSlug = theTitle.replace(/&/g, '-and-')
+                    .replace(/[^a-z0-9-]+/g, '-')
+                    .replace(/\-\-+/g, '-')
+                    .replace(/^-+|-+$/g, '');
+            slugInput.val(theSlug);
+        });
+        <!-- create a slug closes -->
+
+
+    </script>
+@endsection
