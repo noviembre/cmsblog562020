9be4c95557ffee20b954c072b4f7a622d9f6623e
Manu PC win8 <perudragon@hotmail.com>
2020-06-06 08:28:20 -0500

    21 corrigiendo orden del codigo
    creo que no es de importancia pero ordene el codigo similar al que el tutor lo hizo

--- resources/views/backend/blog/table.blade.php
+++ resources/views/backend/blog/table.blade.php
@@ -21,37 +21,40 @@
             <td>{{ $post->title }}</td>
             <td> {{ $post->author->name }} </td>
             <td>{{ $post->category->title }}</td>
+
+
             <td>
+                {!! Form::open([
+                'method' => 'DELETE',
+                 'route' => ['blog.destroy', $post->id ]
+             ]) !!}
+
                 <div class="btn-group">
                     @if (check_user_permissions($request, "Blog@edit", $post->id))
                     <a class="btn btn-info btn-sm" href=" {{ route('blog.edit', ['id' => $post->id ]) }}">                                    <i class="fas fa-edit"></i>
                     </a>
+
                     @else
                         <a href="#" class="btn btn-sm btn-info disabled">
                             <i class="fa fa-edit"></i>
                         </a>
                     @endif
 
-                    {!! Form::open([
-                      'method' => 'DELETE',
-                       'route' => ['blog.destroy', $post->id ]
-                   ]) !!}
-
-                        @if (check_user_permissions($request, "Blog@destroy", $post->id))
-                            <button type="submit" class="btn btn-danger btn-sm">
-                                <i class="fa fa-times"></i>
-                            </button>
-                        @else
-                            <button type="button" onclick="return false;" class="btn btn-sm btn-danger disabled">
-                                <i class="fa fa-times"></i>
-                            </button>
-                        @endif
 
 
-                        {{Form::close()}}
+                    @if (check_user_permissions($request, "Blog@destroy", $post->id))
+                        <button type="submit" class="btn btn-danger btn-sm">
+                            <i class="fa fa-times"></i>
+                        </button>
+                    @else
+                        <button type="button" onclick="return false;" class="btn btn-sm btn-danger disabled">
+                            <i class="fa fa-times"></i>
+                        </button>
+                    @endif
 
 
                 </div>
+                {{Form::close()}}
 
 
             </td>
