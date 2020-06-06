918d65ef270142b94f2c824ffe1d6410447d47a3
Manu PC win8 <perudragon@hotmail.com>
2020-06-06 07:15:21 -0500

    20.3 die dumping Permission
    ahora en la misma ruta backend/blog/create
    te aparecera
    ^ "crud-Blog"

--- app/Http/Middleware/CheckPermissionsMiddleware.php
+++ app/Http/Middleware/CheckPermissionsMiddleware.php
@@ -15,12 +15,33 @@ class CheckPermissionsMiddleware
      */
     public function handle($request, Closure $next)
     {
+        // get current user
+        $currentUser = $request->user();
+
+        // get current action
         $currentActionName = $request->route()->getActionName();
         list($controller, $method) = explode('@', $currentActionName);
 
         $controller = str_replace(["App\\Http\\Controllers\\Backend\\", "Controller"], "", $controller);
 
-        dd("C: $controller M: $method");
+        // check if current user has permission
+        // or if he can do something in the controller
+        $crudPermissionsMap = [
+            'crud' => ['create', 'store', 'edit', 'update', 'destroy', 'restore', 'forceDestroy', 'index', 'view']
+        ];
+
+        // perform an array to loop cru permissions map
+        foreach ($crudPermissionsMap as $permission => $methods)
+        {
+            // if the current method exists in methods list,
+            // we'll check the permission
+            if (in_array($method, $methods))
+            {
+                dd("{$permission}-{$controller}");
+            }
+
+        }
+
         return $next($request);
     }
 }
